b1b529f43927681834cf592415860368f9dcee5c
Mikhail Sharkov <mikhail.sharkov@gcorelabs.com>
2021-07-07 14:50:57 +0300

    WIP

--- asn1crypto/keys.py
+++ asn1crypto/keys.py
@@ -1033,6 +1033,7 @@ class PublicKeyAlgorithmId(ObjectIdentifier):
         '1.3.101.111': 'x448',
         '1.3.101.112': 'ed25519',
         '1.3.101.113': 'ed448',
+        '1.2.643.7.1.1.1.1': 'id-tc26-gost3410-12-256',
     }
 
 
