e10b7740a99a7bd623d63e9d282b226c9543e080
Mikhail Sharkov <mikhail.sharkov@gcorelabs.com>
2021-09-29 15:29:12 +0300

    WIP

--- asn1crypto/keys.py
+++ asn1crypto/keys.py
@@ -1089,8 +1089,8 @@ class PublicKeyInfo(Sequence):
             'ed25519': (OctetBitString, None),
             'ed448': (OctetBitString, None),
             # gostR
-            'id-tc26-gost3410-12-256': (OctetBitString, OctetBitString),
-            'id-tc26-gost3410-12-512': (OctetBitString, OctetBitString),
+            'id-tc26-gost3410-12-256': OctetBitString,
+            'id-tc26-gost3410-12-512': OctetBitString,
         }[algorithm]
 
     _spec_callbacks = {
